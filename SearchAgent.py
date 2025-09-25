33694ebce0e445d31029d4e29e945ff6f75780b8
AnuragKushwaha4 <anuragjnv4@gmail.com>
2025-09-25 15:01:23 +0530

    deployment

--- SearchAgent.py
+++ SearchAgent.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["HF_API_KEY"]=os.getenv("HF_API_KEY")
+os.environ["LANGCHAIN_API_KEY"]   = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["GROQ_API_KEY"]        = st.secrets["GROQ_API_KEY"]
+os.environ["LANGCHAIN_PROJECT"]   = st.secrets["LANGCHAIN_PROJECT"]
+os.environ["LANGCHAIN_TRACING_V2"]= "true"
+os.environ["HF_API_KEY"]          = st.secrets["HF_API_KEY"]
 
 import streamlit as st
 from langchain.callbacks import StreamlitCallbackHandler
