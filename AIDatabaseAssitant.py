5f1ff1263151712bcf125caa856b0a9860abea5e
AnuragKushwaha4 <anuragjnv4@gmail.com>
2025-11-09 19:06:01 +0530

    options created

--- AIDatabaseAssitant.py
+++ AIDatabaseAssitant.py
@@ -16,5 +16,28 @@ from langchain.agents.agent_types import AgentType
 from langchain.sql_database import SQLDatabase
 from sqlalchemy import create_engine
 from langchain_groq import ChatGroq
+import sqlite3
 
 llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+st.title("AI Database Assistant Agent")
+
+LOCAL_DB ="use_localDB"
+MYSQL = "use_MYSQL"
+
+radioOpt = ["Use SQLLITE 3 DB student.db","Connect to your SQL DB"]
+selectedOpt = st.sidebar.radio(label="Choose DB",options=radioOpt)
+
+if(radioOpt.index(selectedOpt)==1):
+    db_uri=MYSQL
+    mysqlHost = st.sidebar.text_input("Enter mysql host name")
+    mysqluser = st.sidebar.text_input("Enter username")
+    mysqlPassword = st.sidebar.text_input("Enter password")
+    mysqlDB = st.sidebar.text_input("Enter Database")
+else:
+    db_uri=LOCAL_DB
+
+
+
+
+
