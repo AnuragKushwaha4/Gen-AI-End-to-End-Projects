b282a74582950a05259616e8df801ad9e41028a4
AnuragKushwaha4 <anuragjnv4@gmail.com>
2025-11-09 18:55:22 +0530

    llm created

--- AIDatabaseAssitant.py
+++ AIDatabaseAssitant.py
@@ -8,3 +8,13 @@ os.environ["LANGCHAIN_TRACING_V2"]="true"
 from pathlib import Path
 
 print(f"../Langchain/Database Manager AI/student.db")
+
+import streamlit as st
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents.agent_types import AgentType
+from langchain.sql_database import SQLDatabase
+from sqlalchemy import create_engine
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
